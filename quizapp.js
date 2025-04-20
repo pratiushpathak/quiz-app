0877abc6165178f533548fc97a5243831d2b6f31
pratiush pathak <pratiushpathak@gmail.com>
2025-04-20 05:41:39 +0530

    finising the project

--- quizapp.js
+++ quizapp.js
@@ -27,14 +27,79 @@ const questions=[
 
 ]
 let index=0;
+let total=questions.length;
+let right=0,
+    wrong=0;
 const queBox= document.getElementById("queBox");
 const optionInputs=document.querySelectorAll('.options')
 
+
 function loadQuestion()
 {
+  if(index===total)
+  {
+     return endQuiz();
+  }
+  reset();
   const data=questions[index];// first object
   queBox.innerText=`${index+1} ${data.que}`;// first question retrieved with question no
+  optionInputs[0].nextElementSibling.innerText=data.a;//retring first option
+  optionInputs[1].nextElementSibling.innerText=data.b;
+  optionInputs[2].nextElementSibling.innerText=data.c;
+  optionInputs[3].nextElementSibling.innerText=data.d;
   
+
+}
+
+function submitQuiz(){
+  const data=questions[index];
+  const ans=getAnswer()
+  if(ans=== data.correct)
+  {
+   right++;
+  }
+  else
+  {
+    wrong++;
+  }
+  index++; // moving to next question
+  loadQuestion();
+
+  return;
+
+
+}
+ 
+function getAnswer()
+{
+  let answer;
+  optionInputs.forEach((input)=>{
+    if(input.checked)
+    {
+      // console.log(input.value)
+      answer=input.value;
+    }
+    
+
+  })
+  return answer;
+
+}
+function reset()
+{
+  optionInputs.forEach((input)=>{
+    input.checked=false;
+
+  })
+
+}
+function endQuiz()
+{
+  document.getElementById("box").innerHTML=
+  `
+  <h3>Thank you for playing the Quiz </h3>
+  <h2>${right} / ${total} is correct</h2>
+  `
 
 }
 loadQuestion()
